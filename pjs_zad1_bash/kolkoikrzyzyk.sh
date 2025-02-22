58c08d2c436a640252b59bd0b7106f5c1c6a0968
szymonbielowka <sbielowka@gmail.com>
2025-02-23 00:16:59 +0100

    bash kolkoikrzyzyk

--- pjs_zad1_bash/kolkoikrzyzyk.sh
+++ pjs_zad1_bash/kolkoikrzyzyk.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Plansza do gry
+
 board=(" " " " " " " " " " " " " " " " " " " ")
 current_player="X"
 is_with_computer=false
 
-# Funkcja do wyświetlania planszy
 display_board() {
     echo " ${board[0]} | ${board[1]} | ${board[2]} "
     echo "---+---+---"
@@ -14,13 +13,12 @@ display_board() {
     echo " ${board[6]} | ${board[7]} | ${board[8]} "
 }
 
-# Funkcja do sprawdzenia wygranej
 check_winner() {
     local b=("${board[@]}")
     local win_conditions=(
-        "0 1 2" "3 4 5" "6 7 8" # wiersze
-        "0 3 6" "1 4 7" "2 5 8" # kolumny
-        "0 4 8" "2 4 6"         # przekątne
+        "0 1 2" "3 4 5" "6 7 8"
+        "0 3 6" "1 4 7" "2 5 8"
+        "0 4 8" "2 4 6"
     )
     for condition in "${win_conditions[@]}"; do
         set -- $condition
@@ -32,7 +30,6 @@ check_winner() {
     echo "false"
 }
 
-# Funkcja sprawdzająca remis
 check_draw() {
     for cell in "${board[@]}"; do
         if [[ $cell == " " ]]; then
@@ -43,7 +40,6 @@ check_draw() {
     echo "true"
 }
 
-# Funkcja zapisywania gry
 save_game() {
     local modified_board=("${board[@]/ /Y}")
     echo "${modified_board[*]}" > save_game.txt
@@ -52,7 +48,6 @@ save_game() {
     sleep 1
 }
 
-# Funkcja wczytywania gry
 load_game() {
     if [[ -f save_game.txt ]]; then
         IFS=" " read -r -a board < <(head -n 1 save_game.txt)
@@ -69,96 +64,73 @@ load_game() {
     sleep 1
 }
 
-# Funkcja minimax do oceny ruchów
-minimax() {
-    local depth=$1
-    local is_maximizing=$2
-    local winner=$(check_winner)
+#minimax() {
+#    local depth=$1
+#    local is_maximizing=$2
+#    local winner=$(check_winner)
+#
+#    if [[ $winner == "X" ]]; then
+#        echo -10
+#        return
+#    elif [[ $winner == "O" ]]; then
+#        echo 10
+#        return
+#    elif [[ $(check_draw) == "true" ]]; then
+#        echo 0
+#        return
+#    fi
+#
+#    local best_score
+#    if [[ $is_maximizing == "true" ]]; then
+#        best_score=-1000
+#        for i in "${!board[@]}"; do
+#            if [[ ${board[$i]} == " " ]]; then
+#                board[$i]="O"
+#                score=$(minimax $((depth + 1)) false)
+#                board[$i]=" "
+#                best_score=$((score > best_score ? score : best_score))
+#            fi
+#        done
+#    else
+#        best_score=1000
+#        for i in "${!board[@]}"; do
+#            if [[ ${board[$i]} == " " ]]; then
+#                board[$i]="X"
+#                score=$(minimax $((depth + 1)) true)
+#                board[$i]=" "
+#                best_score=$((score < best_score ? score : best_score))
+#            fi
+#        done
+#    fi
+#    echo "$best_score"
+#}
 
-    if [[ $winner == "X" ]]; then
-        echo -10 # Gracz wygrywa
-        return
-    elif [[ $winner == "O" ]]; then
-        echo 10 # Komputer wygrywa
-        return
-    elif [[ $(check_draw) == "true" ]]; then
-        echo 0 # Remis
-        return
-    fi
+#computer_move() {
+#    echo "Ruch komputera..."
+#    sleep 1
+#    local best_score=-1000
+#    local best_move=-1
+#
+#    for i in "${!board[@]}"; do
+#        if [[ ${board[$i]} == " " ]]; then
+#            board[$i]="O"
+#            score=$(minimax 0 false)
+#            board[$i]=" "
+#            if [[ $score -gt $best_score ]]; then
+#                best_score=$score
+#                best_move=$i
+#            fi
+#        fi
+#    done
+#
+#    board[$best_move]="O"
+#}
 
-    local best_score
-    if [[ $is_maximizing == "true" ]]; then
-        best_score=-1000
-        for i in "${!board[@]}"; do
-            if [[ ${board[$i]} == " " ]]; then
-                board[$i]="O"
-                score=$(minimax $((depth + 1)) false)
-                board[$i]=" "
-                best_score=$((score > best_score ? score : best_score))
-            fi
-        done
-    else
-        best_score=1000
-        for i in "${!board[@]}"; do
-            if [[ ${board[$i]} == " " ]]; then
-                board[$i]="X"
-                score=$(minimax $((depth + 1)) true)
-                board[$i]=" "
-                best_score=$((score < best_score ? score : best_score))
-            fi
-        done
-    fi
-    echo "$best_score"
-}
-
-computer_move() {
-    echo "Ruch komputera..."
-    sleep 1
-    local best_score=-1000
-    local best_move=-1
-
-    for i in "${!board[@]}"; do
-        if [[ ${board[$i]} == " " ]]; then
-            board[$i]="O"
-            score=$(minimax 0 false)
-            board[$i]=" "
-            if [[ $score -gt $best_score ]]; then
-                best_score=$score
-                best_move=$i
-            fi
-        fi
-    done
-
-    board[$best_move]="O"
-}
-
-# Funkcja gry
 play_game() {
     while true; do
         clear
         echo "Kółko i krzyżyk"
         display_board
-##
-#        if [[ $is_with_computer == "true" && $current_player == "O" ]]; then
-#            computer_move
-#            if [[ $(check_winner) == "true" ]]; then
-#                clear
-#                display_board
-#                echo "Komputer wygrywa!"
-#                sleep 3
-#                > save_game.txt # Czyścimy zapis
-#                break
-#            elif [[ $(check_draw) == "true" ]]; then
-#                clear
-#                display_board
-#                echo "Remis!"
-#                > save_game.txt # Czyścimy zapis
-#                break
-#            fi
-#            current_player="X"
-#            continue
-#        fi
-##
         echo "Gracz $current_player, wybierz pole (0-8) lub 'save' aby zapisać grę lub 'exit' aby zakonczyc:"
         read -r move
         if [[ $move == "save" ]]; then
@@ -178,29 +150,26 @@ play_game() {
             display_board
             echo "Gracz $current_player wygrywa!"
             sleep 3
-            > save_game.txt # Czyścimy zapis
+            > save_game.txt
             break
         elif [[ $(check_draw) == "true" ]]; then
             clear
             display_board
             echo "Remis!"
-            > save_game.txt # Czyścimy zapis
+            > save_game.txt
             break
         fi
         current_player=$([[ $current_player == "X" ]] && echo "O" || echo "X")
     done
 }
 
-# Menu główne
 main_menu() {
     while true; do
         clear
         echo "Kółko i krzyżyk - Menu główne"
         echo "1. Nowa gra"
         echo "2. Wczytaj grę i graj 1v1"
-        echo "3. Nowa gra z komputerem"
-        echo "4. Wczytaj grę i graj z komputerem"
-        echo "5. Wyjście"
+        echo "3. Wyjście"
         read -r choice
         case $choice in
             1)
@@ -215,17 +184,6 @@ main_menu() {
                 play_game
                 ;;
             3)
-                board=(" " " " " " " " " " " " " " " " " " " ")
-                current_player="X"
-                is_with_computer=true
-                play_game
-                ;;
-            4)
-                load_game
-                is_with_computer=true
-                play_game
-                ;;
-            5)
                 echo "Do widzenia!"
                 break
                 ;;
@@ -237,5 +195,4 @@ main_menu() {
     done
 }
 
-# Uruchomienie menu głównego
 main_menu
